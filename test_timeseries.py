23907bffd67c2643a9638995ea3e1cf2ad436dde
Charles Liu <therealcharlesliu@gmail.com>
2016-02-24 22:22:49 -0500

    fix tests

--- test_timeseries.py
+++ test_timeseries.py
@@ -2,7 +2,7 @@ import timeseries as ts
 from pytest import raises
 
 def test_str():
-    assert print(ts.TimeSeries(range(5), range(5,10))) == "TimeSeries with 5 elements: ([0.0, 1.0, 2.0, 3.0, 4.0], [5.0, 6.0, 7.0, 8.0, 9.0])"
+    assert str(ts.TimeSeries(range(5), range(5,10))) == "TimeSeries with 5 elements: ([0.0, 1.0, 2.0, 3.0, 4.0], [5.0, 6.0, 7.0, 8.0, 9.0])"
     
 def test_len_error():
     with raises(AssertionError):
--- timeseries.py
+++ timeseries.py
@@ -26,7 +26,7 @@ class TimeSeries:
     >>> values = list(range(10, 20))
     >>> timeSeries = TimeSeries(times, values)
     >>> print(timeSeries)
-    TimeSeries with 10 elements: ([0, 1, 2, 3, 4, 5, ...], [10, 11, 12, 13, 14, 15, ...])
+    TimeSeries with 10 elements: ([0.0, 1.0, 2.0, 3.0, 4.0, 5.0, ...], [10.0, 11.0, 12.0, 13.0, 14.0, 15.0, ...])
 
     # Accessor methods
     >>> timeSeries = TimeSeries(range(5), range(5,10))
