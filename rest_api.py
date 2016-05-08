a7c1ff34c652fcebb5c8b12eb2240585fcd36112
Charles Liu <therealcharlesliu@gmail.com>
2016-05-07 22:04:43 -0400

    working upsert/insert/select

--- rest_api.py
+++ rest_api.py
@@ -13,7 +13,7 @@ def json_error(handler, code, reason):
     handler.set_status(code)
     handler.finish({"reason":reason})    
 
-class TimeSeriesHandler(tornado.web.RequestHandler):
+class TSHandler(tornado.web.RequestHandler):
     def post(self):
         if not self.request.body:
             json_error(self, 400, reason="Missing data required.")
@@ -42,6 +42,8 @@ class TimeSeriesHandler(tornado.web.RequestHandler):
         o = urlparse(self.request.uri)
         query = parse_qs(o.query)
         additional = {}
+        fields = []
+        metadata_dict = {}
 
         if 'limit' in query and len(query['limit']) == 1:
             additional['limit'] = query.pop('limit')[0]
@@ -56,24 +58,57 @@ class TimeSeriesHandler(tornado.web.RequestHandler):
         if len(additional) == 0:
             additional = None
         
-        fields = []
         if 'fields' in query:
             fields = query.pop('fields')
+        
+        i = 1
+        while "field" + str(i) in query and len(query["field" + str(i)]) == 1:
+            field_name = query["field" + str(i)][0]
+            if "from" + str(i) in query or "to" + str(i) in query:
+                field_params = {}
+                if "from" + str(i) in query and len(query["from" + str(i)]) == 1:
+                    field_params[">="] = query["from" + str(i)][0]
+                if "to" + str(i) in query and len(query["to" + str(i)]) == 1:
+                    field_params["<="] = query["to" + str(i)][0]
+                metadata_dict[field_name] = field_params
+            elif "value" + str(i) in query and len(query["value" + str(i)]) == 1:
+                metadata_dict[field_name] = query["value" + str(i)][0]
+            else:
+                json_error(self, 400, reason="Query for field requires corresponding value, from, or to params, e.g. field1=colname&value1=colvalue")
+                return
+            i += 1
             
-        print(fields)
-        print(additional)
+        try:
+            res = client.select(metadata_dict, fields, additional)
+            self.write({"Status": TSDBStatus(res[0]).name, "Payload":res[1]})
+        except BaseException as e:
+            json_error(self, 400, reason=str(e))
+    
+class TSUpsertHandler(tornado.web.RequestHandler):
+    def post(self):
+        if not self.request.body:
+            json_error(self, 400, reason="Missing data required.")
+            return
+        request = tornado.escape.json_decode(self.request.body)
+        if 'pk' not in request:
+            json_error(self, 400, reason="pk required for upsert.")
+            return
+        if type(request['pk']) != str:
+            json_error(self, 400, reason="pk must be a string.")
+            return
+        pk = request.pop("pk")
         
         try:
-            #new_ts = ts.TimeSeries(request['t'], request['v'])
-            #res = client.insert_ts(request['pk'], new_ts)
-            self.write({"Status": True})
+            res = client.upsert_meta(pk, request)
+            self.write({"Status": TSDBStatus(res[0]).name})
         except BaseException as e:
             json_error(self, 400, reason=str(e))
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/api/timeseries", TimeSeriesHandler)
+            (r"/api/timeseries", TSHandler),
+            (r"/api/timeseries/upsert", TSUpsertHandler)
         ]
         tornado.web.Application.__init__(self, handlers)
 
